8050068e8e028e3088a4c4263e4b9d54dc3058f3
Hritik Khetan <39329476+hritikkhetan@users.noreply.github.com>
2021-06-29 11:52:48 +0530

    Update inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -57,7 +57,7 @@ router.get('/body_style/:body_style/:num', async(req, res) => {
     }
 })
 
-router.get('/advance-search/basic/:company/:model/:trim/:year/:lprice/:hprice/:lmileage/:hmileage/:num_cylinders/:cylinders/:condition/:num', async(req, res) => {
+router.get('/advance-search/:company/:model/:trim/:year/:lprice/:hprice/:lmileage/:hmileage/:num_cylinders/:cylinders/:condition/:ext_color/:int_color/:ext_type/:num', async(req, res) => {
     try{
         const company = req.params.company
         const model = req.params.model
@@ -70,14 +70,17 @@ router.get('/advance-search/basic/:company/:model/:trim/:year/:lprice/:hprice/:l
         const hprice = req.params.hprice
         const lmileage = req.params.lmileage
         const hmileage = req.params.hmileage
+        const ext_color = req.params.ext_color
+        const int_color = req.params.int_color
+        const ext_type = req.params.ext_type
         const num = req.params.num
         var car = new Car()
         if(num == 1)
-            car = await Car.find({$and:[{'company': company}, {'model': model}, {'trim': trim}, {'year': year}, {'price': {$gte: lprice, $lte: hprice}}, {'mileage': {$gte: lmileage, $lte: hmileage}}, {'num_cylinders': num_cylinders}, {'cylinders': cylinders}, {'condition': condition}]}).sort({_id:-1}).limit(2)
+            car = await Car.find({$and:[{'company': company}, {'model': model}, {'trim': trim}, {'year': year}, {'price': {$gte: lprice, $lte: hprice}}, {'mileage': {$gte: lmileage, $lte: hmileage}}, {'num_cylinders': num_cylinders}, {'cylinders': cylinders}, {'condition': condition}, {'ext_color': ext_color}, {'int_color': int_color}, {'ext_type': ext_type}]}).sort({_id:-1}).limit(2)
         else if(num == 2)
-            car = await Car.find({$and:[{'company': company}, {'model': model}, {'trim': trim}, {'year': year}, {'price': {$gte: lprice, $lte: hprice}}, {'mileage': {$gte: lmileage, $lte: hmileage}}, {'num_cylinders': num_cylinders}, {'cylinders': cylinders}, {'condition': condition}]}).sort({price: 1}).limit(2)
+            car = await Car.find({$and:[{'company': company}, {'model': model}, {'trim': trim}, {'year': year}, {'price': {$gte: lprice, $lte: hprice}}, {'mileage': {$gte: lmileage, $lte: hmileage}}, {'num_cylinders': num_cylinders}, {'cylinders': cylinders}, {'condition': condition}, {'ext_color': ext_color}, {'int_color': int_color}, {'ext_type': ext_type}]}).sort({price: 1}).limit(2)
         else
-            car = await Car.find({$and:[{'company': company}, {'model': model}, {'trim': trim}, {'year': year}, {'price': {$gte: lprice, $lte: hprice}}, {'mileage': {$gte: lmileage, $lte: hmileage}}, {'num_cylinders': num_cylinders}, {'cylinders': cylinders}, {'condition': condition}]}).sort({price: -1}).limit(2)
+            car = await Car.find({$and:[{'company': company}, {'model': model}, {'trim': trim}, {'year': year}, {'price': {$gte: lprice, $lte: hprice}}, {'mileage': {$gte: lmileage, $lte: hmileage}}, {'num_cylinders': num_cylinders}, {'cylinders': cylinders}, {'condition': condition}, {'ext_color': ext_color}, {'int_color': int_color}, {'ext_type': ext_type}]}).sort({price: -1}).limit(2)
         res.json({
             "data": car,
             "message": "List of all cars based on user search request",
@@ -89,7 +92,7 @@ router.get('/advance-search/basic/:company/:model/:trim/:year/:lprice/:hprice/:l
     }
 })
 
-router.get('/advance-search/custom/:company/:model/:trim/:year/:ext_color/:int_color/:ext_type/:num', async(req, res) => {
+/*router.get('/advance-search/custom/:company/:model/:trim/:year/:ext_color/:int_color/:ext_type/:num', async(req, res) => {
     try{
         const company = req.params.company
         const model = req.params.model
@@ -115,6 +118,6 @@ router.get('/advance-search/custom/:company/:model/:trim/:year/:ext_color/:int_c
     }catch(err){
         res.send('Error ' + err)
     }
-})
+})*/
 
 module.exports = router
